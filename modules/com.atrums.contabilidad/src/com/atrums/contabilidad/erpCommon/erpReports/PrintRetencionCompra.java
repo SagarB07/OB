86e4874fdf9c9ac38d49c1f3a89d355c5adbedd6
Santiago Yacelga <esyacelga@hotmail.com>
2016-03-03 17:19:41 +0100

    temoporaL

--- modules/com.atrums.contabilidad/src/com/atrums/contabilidad/erpCommon/erpReports/PrintRetencionCompra.java
+++ modules/com.atrums.contabilidad/src/com/atrums/contabilidad/erpCommon/erpReports/PrintRetencionCompra.java
@@ -25,9 +25,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.openbravo.base.secureApp.VariablesSecureApp;
-
-import com.atrums.reporting.utility.reporting.DocumentType;
-import com.atrums.reporting.utility.reporting.printing.PrintController;
+import org.openbravo.erpCommon.utility.reporting.printing.PrintController;
+import org.openbravo.model.common.enterprise.DocumentType;
 
 @SuppressWarnings("serial")
 public class PrintRetencionCompra extends PrintController
@@ -49,22 +48,22 @@ public class PrintRetencionCompra extends PrintController
     public void doPost(HttpServletRequest request, HttpServletResponse response)
 	    throws IOException, ServletException
     {
-	VariablesSecureApp vars = new VariablesSecureApp(request);
-
-	DocumentType documentType = DocumentType.RETENCION;
-	// The prefix PRINTINVOICES is a fixed name based on the KEY of the
-	// AD_PROCESS
-	String sessionValuePrefix = "PRINTRETENCION";
-	String strDocumentId = null;
-
-	strDocumentId = vars.getSessionValue(sessionValuePrefix
-		+ ".inpcoRetencionCompraId_R");
-	if (strDocumentId.equals(""))
-	    strDocumentId = vars.getSessionValue(sessionValuePrefix
-		    + ".inpcoretencioncompraid");
-
-	post(request, response, vars, documentType, sessionValuePrefix,
-	     strDocumentId);
+//	VariablesSecureApp vars = new VariablesSecureApp(request);
+//
+//	DocumentType documentType = DocumentType.RETENCION;
+//	// The prefix PRINTINVOICES is a fixed name based on the KEY of the
+//	// AD_PROCESS
+//	String sessionValuePrefix = "PRINTRETENCION";
+//	String strDocumentId = null;
+//
+//	strDocumentId = vars.getSessionValue(sessionValuePrefix
+//		+ ".inpcoRetencionCompraId_R");
+//	if (strDocumentId.equals(""))
+//	    strDocumentId = vars.getSessionValue(sessionValuePrefix
+//		    + ".inpcoretencioncompraid");
+//
+//	post(request, response, vars, documentType, sessionValuePrefix,
+//	     strDocumentId);
     }
 
     public String getServletInfo()
