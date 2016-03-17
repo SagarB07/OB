a5f6999dd392d584793bf6a966315eca55bd7e98
Santiago Yacelga <esyacelga@hotmail.com>
2016-03-17 17:21:57 +0100

    MODIFICACION TABLA CONTRATOS

--- Documentos/TABLA_IDT_CONTRATO.sql
+++ Documentos/TABLA_IDT_CONTRATO.sql
@@ -9,7 +9,7 @@ CREATE TABLE idt_contrato
   c_currency_id character varying(32), 
   fin_financial_account_id character varying(32),
   fin_paymentmethod_id character varying(32),
-  ne_area_empresa_id character varying(32),
+  area_empresa_id character varying(32),
   createdby character varying(32) NOT NULL,
   updatedby character varying(32) NOT NULL,
   isactive character(1) NOT NULL DEFAULT 'Y'::bpchar,
@@ -23,14 +23,63 @@ CREATE TABLE idt_contrato
   pagofondoreserva character(1) NOT NULL,  
   is_impuesto_asumido character(1),
   aplica_utilidad character(1) DEFAULT 'Y'::bpchar,
-  em_ne_sissalnet character varying(60),  
-  em_ne_motivo_salida character varying(60),
-  em_ne_is_jornada_parcial character(1) DEFAULT 'N'::bpchar,
-  em_ne_num_horas_parciales numeric,
-  em_ne_vacacion_prop numeric(10,2),
-  em_ne_vacacion_tom numeric(10,2),
-  em_ne_vacacion_res numeric(10,2),
-  em_ne_region character varying(60),
-  em_ne_observaciones character varying(255),
+  ne_sissalnet character varying(60),  
+  ne_motivo_salida character varying(60),
+  ne_is_jornada_parcial character(1) DEFAULT 'N'::bpchar,
+  ne_num_horas_parciales numeric,
+  ne_vacacion_prop numeric(10,2),
+  ne_vacacion_tom numeric(10,2),
+  ne_vacacion_res numeric(10,2),
+  ne_region character varying(60),
+  ne_observaciones character varying(255),
+  constraint idt_idtcontrato_pk primary key (idt_contrato_id)
+  );
+  create unique index idt_contrato_PK on idt_contrato (
+	idt_contrato_id
+	);
+
+	
+alter table idt_contrato
+   add constraint IDT_FK_contrato_c_bpartner foreign key (c_bpartner_id)
+      references c_bpartner (c_bpartner_id)
+      on delete restrict on update restrict;
+	  
+alter table idt_contrato
+   add constraint IDT_FK_contrato_atnorh_cargo foreign key (atnorh_cargo_id)
+      references atnorh_cargo (atnorh_cargo_id)
+      on delete restrict on update restrict;
   
-  )
+ alter table idt_contrato
+   add constraint IDT_FK_contrato_ad_client foreign key (ad_client_id)
+      references ad_client (ad_client_id)
+      on delete restrict on update restrict;
+	  
+alter table idt_contrato
+   add constraint IDT_FK_contrato_ad_org foreign key (ad_org_id)
+      references ad_org (ad_org_id)
+      on delete restrict on update restrict;	  
+
+alter table idt_contrato
+   add constraint IDT_FK_contrato_ad_org foreign key (c_doctype_id)
+      references ad_org (c_doctype_id)
+      on delete restrict on update restrict;	  
+	  
+	  
+alter table idt_contrato
+   add constraint IDT_FK_contrato_c_currency foreign key (c_currency_id)
+      references c_currency (c_currency_id)
+      on delete restrict on update restrict;	  
+	  
+	  
+alter table idt_contrato
+   add constraint IDT_FK_contrato_c_currency foreign key (fin_financial_account_id)
+      references fin_financial_account (fin_financial_account_id)
+      on delete restrict on update restrict;		  
+  
+ alter table idt_contrato
+   add constraint IDT_FK_contrato_fin_paymentmethod foreign key (fin_paymentmethod_id)
+      references fin_paymentmethod (fin_paymentmethod_id)
+      on delete restrict on update restrict;		  
+	  
+
+	  
