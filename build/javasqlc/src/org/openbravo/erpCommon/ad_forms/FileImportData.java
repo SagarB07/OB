9d6d9f77f780fbe92941be5348af73e547699b06
Santiago Yacelga <esyacelga@hotmail.com>
2016-03-18 15:30:40 +0100

    Importacion de contratos

--- build/javasqlc/src/org/openbravo/erpCommon/ad_forms/FileImportData.java
+++ build/javasqlc/src/org/openbravo/erpCommon/ad_forms/FileImportData.java
@@ -107,9 +107,44 @@ static Logger log4j = Logger.getLogger(FileImportData.class);
 	        ignore.printStackTrace();
 	      }
 	    }
-
 	    return datoCampo;
 	  }  
+public static String obtenerIDCampo (Connection conn, ConnectionProvider connectionProvider, String strSql, String campo )    throws ServletException {
+	String datoCampo= "";
+	ResultSet result;
+    PreparedStatement st = null;
+    try {
+    st = connectionProvider.getPreparedStatement(strSql);
+    result = st.executeQuery();
+    campo= campo.replace(" ", "");
+    campo= campo.replace("=", "");
+    boolean continueResult = true;
+    while(continueResult && result.next()) {
+        datoCampo = UtilSql.getValue(result, campo);
+        if (datoCampo!= ""){
+        	continueResult= false;
+        }
+      }
+      result.close();
+    } catch(SQLException e){
+      log4j.error("SQL error in query: " + strSql + "Exception:"+ e);
+      throw new ServletException("@CODE=" + Integer.toString(e.getErrorCode()) + "@" + e.getMessage());
+    } catch(Exception ex){
+      log4j.error("Exception in query: " + strSql + "Exception:"+ ex);
+      throw new ServletException("@CODE=@" + ex.getMessage());
+    } finally {
+      try {
+        connectionProvider.releasePreparedStatement(st);
+      } catch(Exception ignore){
+        ignore.printStackTrace();
+      }
+    }
+
+    return datoCampo;
+
+	
+}
+  
   
   
 public static String obtenerIDCampoRubro(Connection conn, ConnectionProvider connectionProvider,String name)    throws ServletException {
@@ -124,12 +159,8 @@ public static String obtenerIDCampoRubro(Connection conn, ConnectionProvider con
 	    try {
 	    st = connectionProvider.getPreparedStatement(strSql);
 	    result = st.executeQuery();
-	      
-	      boolean continueResult = true;
-
-	      while(continueResult && result.next()) {
-	        	        
-	        datoCampo = UtilSql.getValue(result, "no_tipo_ingreso_egreso_id");
+        boolean continueResult = true;
+        while(continueResult && result.next()) {
 	        datoCampo = UtilSql.getValue(result, "no_tipo_ingreso_egreso_id");
 	        if (datoCampo!= ""){
 	        	continueResult= false;
