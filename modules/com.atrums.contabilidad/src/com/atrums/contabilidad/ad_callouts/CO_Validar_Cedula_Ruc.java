5d6d7ff2f3088f202b72cfd4d03a366cc0075c6a
Santiago Yacelga <esyacelga@hotmail.com>
2016-03-24 12:30:06 +0100

    COMPROBACION DE ERRORES AL 70 %

--- modules/com.atrums.contabilidad/src/com/atrums/contabilidad/ad_callouts/CO_Validar_Cedula_Ruc.java
+++ modules/com.atrums.contabilidad/src/com/atrums/contabilidad/ad_callouts/CO_Validar_Cedula_Ruc.java
@@ -95,8 +95,6 @@ public class CO_Validar_Cedula_Ruc extends HttpSecureAppServlet {
   private String validarDocumento(VariablesSecureApp vars, String numero, String tipoIdentificacion) {
     boolean valor = true;
     String mensaje = "";
-    
-    
 
     if (tipoIdentificacion.equals("02") || tipoIdentificacion.equals("01")) {
 
