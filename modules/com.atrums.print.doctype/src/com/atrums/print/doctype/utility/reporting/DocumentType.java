dfa38601941a55b17d5b6ca48a67903fbeb04747
Santiago Yacelga <esyacelga@hotmail.com>
2016-04-15 09:24:29 -0500

    PRINT DOCTYPE

---
+++ modules/com.atrums.print.doctype/src/com/atrums/print/doctype/utility/reporting/DocumentType.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ * ************************************************************************ The
+ * contents of this file are subject to the Openbravo Public License Version 1.1
+ * (the "License"), being the Mozilla Public License Version 1.1 with a
+ * permitted attribution clause; you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.openbravo.com/legal/license.html Software distributed under the
+ * License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing rights and limitations under the License. The Original Code is
+ * Openbravo ERP. The Initial Developer of the Original Code is Openbravo SLU
+ * All
+ * portions are Copyright (C) 2001-2010 Openbravo SLU All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ * ***********************************************************************
+ */
+package com.atrums.print.doctype.utility.reporting;
+
+public enum DocumentType {
+  UNKNOWN(null, null), COTIZACION("LDT_COTIZACION", "cotizaciones/"), ROUTINGORDER(
+      "LDT_ROUTING_ORDER", "routingorder/");
+
+  private String _tableName;
+  private String _contextSubFolder;
+
+  private DocumentType(String tableName, String contextSubFolder) {
+    _tableName = tableName;
+    _contextSubFolder = contextSubFolder;
+  }
+
+  public String getTableName() {
+    return _tableName;
+  }
+
+  public String getContextSubFolder() {
+    return _contextSubFolder;
+  }
+}
